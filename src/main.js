685bb0a35a32e8b6a016b064585dca641d5281a9
Fatima <tona14520@gmail.com>
2021-09-10 17:11:09 +0200

    new commit

--- src/main.js
+++ src/main.js
@@ -5,10 +5,18 @@ import router from './router'
 import CoreuiVue from '@coreui/vue'
 import { iconsSet as icons } from './assets/icons/icons.js'
 import store from './store'
+import * as VueGoogleMaps from "vue2-google-maps";
 
+Vue.config.productionTip = false
 Vue.config.performance = true
 Vue.use(CoreuiVue)
 Vue.prototype.$log = console.log.bind(console)
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "Google_key",
+    libraries: "places",
+  },
+});
 
 new Vue({
   el: '#app',
