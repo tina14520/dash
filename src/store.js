2011d4e72676771adbc13a6c715d6dcf1589f3c6
Anas Alwaleed <anasalwaleed77@gmail.com>
2021-10-04 15:25:10 +0200

    m

--- src/store.js
+++ src/store.js
@@ -4,7 +4,30 @@ Vue.use(Vuex)
 
 const state = {
   sidebarShow: 'responsive',
-  sidebarMinimize: false
+  sidebarMinimize: false,
+  userState: "",
+    city: "",
+    street: "",
+    email: undefined,
+    validation: undefined,
+    progressNow: '0%',
+    currentStep: 1,
+    steps: {
+      step1: {
+        selectedClientPhone: null,
+        selectedClientId: null,
+      },
+      step2: {
+        pickUpLocationLat: null,
+        pickUpLocationLng: null,
+      },
+      step3: {
+        dropOffLocationLat: null,
+        dropOffLocationLng: null,
+      },
+      step4: {},
+      step5:{}
+    },
 }
 
 const mutations = {
@@ -18,6 +41,37 @@ const mutations = {
   },
   set (state, [variable, value]) {
     state[variable] = value
+  },
+  // Steps & Progress
+  updateCurrentStep(state, payload) {
+    state.currentStep = payload.currentStep
+  },
+  setProgress(state, payload) {
+    state.progressNow = payload.progressNow + "%"
+  },
+
+  // Step 1
+  setClient(state, payload) {
+    state.steps.step1.selectedClientPhone = payload.selectedClientPhone
+  },
+  setClientId(state, payload) {
+    state.steps.step1.selectedClientId = payload.selectedClientId
+  },
+
+  // Step 2
+  setPickUpLocationLat(state, payload) {
+    state.steps.step2.pickUpLocationLat = payload.pickUpLocationLat
+  },
+  setPickUpLocationLng(state, payload) {
+    state.steps.step2.pickUpLocationLng = payload.pickUpLocationLng
+  },
+
+  // Step 3
+  setDropOffLocationLat(state, payload) {
+    state.steps.step3.dropOffLocationLat = payload.dropOffLocationLat
+  },
+  setDropOffLocationLng(state, payload) {
+    state.steps.step3.dropOffLocationLng = payload.dropOffLocationLng
   }
 }
 
